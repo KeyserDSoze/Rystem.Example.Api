27697793a271687ad36e23312b756ba4618faf1e
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2023-06-17 14:36:53 +0200

    Add project files.

---
+++ ReactAppApi/Models/UserOne.cs
@@ -0,0 +1,16 @@
+﻿namespace ReactAppApi.Models
+{
+    public sealed class UserOne
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public List<Settings> Settings { get; set; }
+    }
+    public sealed class Settings
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public List<string> Others { get; set; }
+    }
+}
